20072c05f6a949bdab815ac7d0eed99c7f54740f
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2015-08-08 11:05:11 -0700

    Going to see slough feg tonight. gonna be super rad.

--- build.js
+++ build.js
@@ -4,8 +4,12 @@ var htmlAutoprefixer = require('html-autoprefixer'),
     sourceHtmlFile,
     prefixedHtmlFile;
 
+console.log(' Build Gk Product Gallery');
+
 sourceHtmlFile = fs.readFileSync(path.join(process.cwd(), 'source', 'gkProductGallery.html'), { encoding : 'utf8'});
 
 prefixedHtmlFile = htmlAutoprefixer.process(sourceHtmlFile);
 
 fs.writeFileSync(path.join(process.cwd(), 'dist', 'gkProductGallery-prefixed.html'), prefixedHtmlFile);
+
+console.log('Complete :  Build Gk Product Gallery');
